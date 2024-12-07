3a9394dbfa19e6865798aecb800cb3698edeb108
HeifaHr <hraiechheifa@gmail.com>
2024-12-07 23:44:15 +0100

    expenses-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import './App.css'
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./Components/SignUp";
 import Login from './Components/LogIn';
+import ExpensesForm from './Components/Expenses/ExpensesForm';
+import AddCategory from './Components/Expenses/AddCategory';
 
 function App() {
+  const [categories, setCategories] = useState(['Food', 'Transport', 'Entertainment']);
 
 
   return (
@@ -15,7 +17,8 @@ function App() {
       {/* Default Route: Sign Up */}
       <Route path="/" element={<SignUp />} />
       <Route path="/Login" element={<Login />} />
-
+      <Route path="/expenses-form" element={<ExpensesForm categories={categories} setCategories={setCategories} />} />
+      <Route path="/add-category" element={<AddCategory categories={categories} setCategories={setCategories} />} />
     </Routes>
   </Router>
   )
